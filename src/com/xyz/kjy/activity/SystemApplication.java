070fd44c27cb8e52c685e9683dca1720e300fc93
ZhenShiErGe <895990111@qq.com>
2015-10-25 11:46:28 +0800

    完成注销模块

--- src/com/xyz/kjy/activity/SystemApplication.java
+++ src/com/xyz/kjy/activity/SystemApplication.java
@@ -6,7 +6,11 @@ import java.util.List;
 import android.app.Activity; 
 import android.app.Application; 
  
-
+/**
+ * 此类用于退出android主程序
+ * @author xuyizhen
+ *
+ */
 public class SystemApplication extends Application {
     private List<Activity> mList = new LinkedList<Activity>(); 
     private static SystemApplication instance; 
